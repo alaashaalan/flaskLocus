2c39d855a55c23950aa9889efe2b9869ef21bd50
Alaa Shaalan <alaashaalan@d142-058-221-055.wireless.sfu.ca>
2017-04-27 15:04:17 -0700

    Added Server Config

---
+++ config.py
@@ -0,0 +1,15 @@
+def credentials:
+
+	host="localhost"
+	user="root"
+	passwd="12345"
+	db="locus_development"
+
+	db_credentials = {
+		'host' : host
+		'user' : user
+		'passwd' : passwd
+		'db' : db
+	}
+
+	return db_credentials
--- db.py
+++ db.py
@@ -1,17 +1,15 @@
-from tinydb import TinyDB, where, Query
 import pprint
 import MySQLdb
+import config
 
-#prepare database
-#db = TinyDB('db.json')
-#db.purge_table('_default')  # this is unnecessaryly risky
-#locus_data = db.table('locus_data')
+
 
 def connection():
-	conn = MySQLdb.connect(host="localhost",
-		user="root",
-		passwd="12345",
-		db="locus_development")
+	credentials = credentials()
+	conn = MySQLdb.connect(host=credentials['host'],
+		user=credentials['user'],
+		passwd=credentials['passwd'],
+		db=credentials['db'])
 
 	c = conn.cursor();
 	
@@ -33,49 +31,39 @@ def insert(row):
 	conn.close()
 
 
+def find_tag_id(tag_id):
+	Beacon = Query()
+	beacons_with_tag = locus_data.search(Beacon.tag_id == tag_id)
 
+    # for beacon in beacons_with_tag:
 
-# def insert(row):
-# 	if len(row) == 0:
-# 		pass
-# 	else: 
-# 		locus_data.insert(row)
+	return beacons_with_tag
 
-
-# def find_tag_id(tag_id):
-# 	Beacon = Query()
-# 	beacons_with_tag = locus_data.search(Beacon.tag_id == tag_id)
-
-# 	# for beacon in beacons_with_tag:
-# 		# print(beacon['rssi'])
-
-# 	return beacons_with_tag
-
-# def convert_to_csv(list_of_records):
-# 	csv_result = ""
-# 	for row in list_of_records:
-# 		try:
-# 			csv_result += ','.join([row['time_stamp'], row['report_type'], row['tag_id'], row['gateway_id'], row['rssi'], row['raw_packet_content']]) + '\n'
-# 		except KeyError, e:
-# 			csv_result += ','.join(['', row['report_type'], row['tag_id'], row['gateway_id'], row['rssi'], row['raw_packet_content']]) + '\n'
+def convert_to_csv(list_of_records):
+ 	csv_result = ""
+ 	for row in list_of_records:
+ 		try:
+ 			csv_result += ','.join([row['time_stamp'], row['report_type'], row['tag_id'], row['gateway_id'], row['rssi'], row['raw_packet_content']]) + '\n'
+ 		except KeyError, e:
+ 			csv_result += ','.join(['', row['report_type'], row['tag_id'], row['gateway_id'], row['rssi'], row['raw_packet_content']]) + '\n'
  
 
-# 	return csv_result
+ 	return csv_result
 
 
-# def pretty_print(list_of_records):
-# 	pp = pprint.PrettyPrinter(indent=4)
-# 	for row in list_of_records:
-# 		pp.pprint(row)
-
-
-
+def pretty_print(list_of_records):
+ 	pp = pprint.PrettyPrinter(indent=4)
+ 	for row in list_of_records:
+ 		pp.pprint(row)
 
 
 
-# # this is only executed if called explicitly. For debugging purposes only
-# if __name__ == '__main__':
-# 	convert_to_csv()
-# 	pretty_print()
 
-# 	print(convert_to_csv_list(find_tag_id('4129B7F0EF39'))) 
+
+
+# this is only executed if called explicitly. For debugging purposes only
+ if __name__ == '__main__':
+ 	convert_to_csv()
+ 	pretty_print()
+
+ 	print(convert_to_csv_list(find_tag_id('4129B7F0EF39'))) 
