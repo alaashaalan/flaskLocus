68a4761c11861253cb4c49f7ec209066026bdf45
Alaa Shaalan <alaashaalan@d142-058-221-055.wireless.sfu.ca>
2017-05-04 15:19:53 -0700

    Name Change

--- db.py
+++ db.py
@@ -6,18 +6,18 @@ import config
 # Returns credentials to connect to database
 def connection():
 	credentials = config.credentials()
-	connection = MySQLdb.connect(host=credentials['host'],
+	database = MySQLdb.connect(host=credentials['host'],
 		user=credentials['user'],
 		passwd=credentials['passwd'],
 		db=credentials['db'])
 
-	cursor = connection.cursor();
+	cursor = database.cursor();
 	
-	return connection, cursor
+	return database, cursor
 
 # Inserts newly collected data to table raw_data
 def insert_raw_data(row):
-	connection, cursor = connection();
+	database, cursor = connection();
 	insert_statement = (
 		"INSERT INTO raw_data (time_stamp, tag_id, gateway_id, rssi, raw_packet_content)"
 		"VALUES (%s, %s, %s, %s, %s)"
@@ -26,26 +26,26 @@ def insert_raw_data(row):
 
 	cursor.execute(insert_statement, data)
 
-	connection.commit()
-	connection.close()
+	database.commit()
+	database.close()
 
 # Finds specific beacons based on their id from raw_data table
 def find_by_tag_id(table_name, fields, tag_id):
-	connection, cursor = connection();
+	database, cursor = connection();
 	query ="SELECT %s FROM %s WHERE tag_id = %s" %(fields, table_name, '%s')
 	cursor.execute(query, [tag_id])
 	records = cursor.fetchall()
-	connection.close()
+	database.close()
 
 	return records
 
 # Finds all records between a specific time frame for a specific table (STRING INPUTS)
 def find_by_datetime_range(table_name, fields, time_stamp_start, time_stamp_end):
-	connection, cursor = connection();
+	database, cursor = connection();
 	query = "SELECT %s FROM %s WHERE time_stamp >= %s AND time_stamp <= %s" % (fields, table_name, '%s', '%s')
 	cursor.execute(query, [time_stamp_start, time_stamp_end])
 	records = cursor.fetchall()
-	connection.close()
+	database.close()
 
 	return records
 
