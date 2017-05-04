dd0a27f37dc349f9ba142fb44416daa885aa975d
Tim Nosov <timnosov@gmail.com>
2017-05-03 22:33:10 -0700

    added timestamp_matching route

--- init.py
+++ init.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from helper_functions import process_message
+import helper_functions
 import db
 import datetime
 
@@ -16,7 +16,7 @@ def index():
 def intake():
 	data = request.get_data()
 	data = str(datetime.datetime.now()).split('.')[0]+','+data 
-	processed_message = process_message(data)
+	processed_message = helper_functions.process_message(data)
 	if processed_message:
 		db.insert_raw_data(processed_message)
 
@@ -33,3 +33,18 @@ def login():
 	 	do_the_login()
 	else: 
 		show_the_login_form()
+
+
+
+@app.route('/timestamp_matching', methods=['GET'])
+def timestamp_matching():
+	start_time = '2017-05-01 17:30:20'
+	end_time = '2017-05-01 17:30:25'
+	tag_id = "5826D8AF76AB"
+	gateway_ids = ["1","2","3"]
+
+	start_time = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
+	end_time = datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
+
+	result = helper_functions.timestamp_matching(start_time, end_time, tag_id, gateway_ids)
+	return  str(result)
