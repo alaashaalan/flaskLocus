f9cf8364af96a94059d93cf27dd76c982a66a95d
Tim Nosov <timnosov@gmail.com>
2017-07-11 14:03:57 -0700

    added slope limit function

--- helper_functions.py
+++ helper_functions.py
@@ -4,10 +4,40 @@ import db
 import trilateration_mysql
 import datetime
 
+RSSI_1M = -60
+
 def rssi_to_meter(rssi): #code works but will need some modification based what type of string we pass it
-	RSSI_1m = -53.16667  #this value is experimentally measured
+	RSSI_1m = RSSI_1M  #this value is experimentally measured
 	distance = 10**((RSSI_1m - rssi)/20)
 	return distance
+
+
+def meter_to_rssi(meter): #code works but will need some modification based what type of string we pass it
+	RSSI_1m = RSSI_1M  #this value is experimentally measured
+	rssi = -20*(math.log10(meter)) + RSSI_1m
+	return rssi
+
+
+def slope_limit_rssi(current_rssi, last_distance, max_delta_distance):
+	if (max_delta_distance>10):
+		new_rssi = current_rssi
+		return new_rssi
+
+	min_rssi = meter_to_rssi(last_distance + max_delta_distance)
+
+	if ((last_distance - max_delta_distance)<=0): 
+		max_rssi = max(current_rssi, meter_to_rssi(last_distance))
+	else: 
+		max_rssi = meter_to_rssi(last_distance - max_delta_distance)
+
+	if((min_rssi<=current_rssi) & (current_rssi<=max_rssi)):
+		new_rssi = current_rssi
+	elif(min_rssi>current_rssi):
+		new_rssi = min_rssi
+	else: 
+		new_rssi = max_rssi
+		
+	return new_rssi
 
 
 def timestamp_matching():
