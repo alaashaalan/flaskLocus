877c02a28f62b6f24fc089dcd4a73c88e629d60e
Anmol Sooch <anmolsooch@d192-075-242-213.wireless.sfu.ca>
2017-08-29 11:47:41 -0700

    this has path locking function

--- helper_functions.py
+++ helper_functions.py
@@ -38,53 +38,43 @@ def slope_limit_rssi(current_rssi, last_distance, max_delta_distance):
 		
 	return new_rssi
 
-# def path_rules(prediction, probabilites, positions):
-# 	for item in range(len(probabilites)-1):
-# 		t = 1
-# 		cur_pos  = prediction[item]
-# 		cur_row = int(cur_pos[0])
-# 		cur_col = int(cur_pos[2])
-		
-# 		next_pos = prediction[item+1]
-# 		next_row = int(next_pos[0])
-# 		next_col = int(next_pos[2])
-
-# 		next_prob = probabilites[item+1]
-# 		next_prob_sorted = sorted(probabilites[item+1], key=int, reverse=True)
-
-# 		while (abs(next_row-cur_row)>1) or (abs(next_col-cur_col)>1): 
-# 			next_best_guess = next_prob_sorted[t]
-# 			for location in range(len(next_prob)):
-# 				if next_best_guess == next_prob[location]: 
-# 					next_pos = positions[location]
-# 			next_row = int(next_pos[0])
-# 			next_col = int(next_pos[2])
-# 			t = t+1
-# 		next_pos = str(next_row)
-# 		next_pos+='-'
-# 		next_pos+=str(next_col)
-# 		prediction[item+1] = next_pos
-# 	return prediction
-
 def path_rules(prediction, probabilites, positions):
+	numb_of_corrections =0
+	last_num = 500
 	for item in range(len(probabilites)-1):
+		t = 1
 		cur_pos  = prediction[item]
+		cur_row = int(cur_pos[0])
+		cur_col = int(cur_pos[2])
+
 		next_pos = prediction[item+1]
-		changes = 0
-		u = zip(cur_pos, next_pos)
-		for i, j in u: 
-			if i != j:
-				changes = changes +1
-		if changes >1: 
-			row = cur_pos[0]
-			col = cur_pos[2]
-			guesses = probabilites[item+1]
-			guesses_sorted = sorted(probabilites[item+1], key=int, reverse=True)
-			next_best_guess =guesses_sorted[1]
-			for i in range(len(guesses)):
-				if next_best_guess == guesses[i]:
-					location = i
-			prediction[item+1]= positions[location]
-			#print "changed item", item+2
-		changes =0
-	return prediction
+		next_row = int(next_pos[0])
+		next_col = int(next_pos[2])
+
+
+		next_prob = probabilites[item+1]
+		next_prob_sorted = sorted(probabilites[item+1], key=int, reverse=True)
+
+		while (abs(next_row-cur_row)>1) or (abs(next_col-cur_col)>1): 
+			next_best_guess = next_prob_sorted[t]
+			for location in range(len(next_prob)):
+				if next_best_guess == next_prob[location]: 
+					next_pos = positions[location]
+			next_row = int(next_pos[0])
+			next_col = int(next_pos[2])
+			t = t+1
+		if t>1: 
+			if abs(last_num - item) ==1:
+				numb_of_corrections+=1
+				last_num=item
+			else: 
+				numb_of_corrections=0
+		next_pos = str(next_row)
+		next_pos+='-'
+		next_pos+=str(next_col)
+		if numb_of_corrections < 2:
+			prediction[item+1] = next_pos
+		else: 
+			numb_of_corrections=0
+
+	print prediction
