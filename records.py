d98e41e498994f5d14622b0df37e9c6b94fa96a4
Tim Nosov <timnosov@gmail.com>
2017-09-25 15:56:41 -0700

    fail gracefully error when list of records is empty

--- records.py
+++ records.py
@@ -87,6 +87,10 @@ class ListOfRecords(list):
 
 			self.append(record)
 
+		if len(self) == 0:
+			raise ValueError("db doesn't contain records for: ", beacon, gateways, start, end, label)
+
+
 
 	def get_rssis(self):
 		rssis = []
