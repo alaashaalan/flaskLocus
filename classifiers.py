9dc8f74c8d1ef1ddf8e0380beb52f1b747f7dffa
Tim Nosov <timnosov@gmail.com>
2017-09-29 17:23:22 -0700

    removed label and filter length from use classifier

--- classifiers.py
+++ classifiers.py
@@ -15,13 +15,12 @@ def create_classifier(beacon_id, gateway_list, start_date, end_date, filter_wind
 
 	return training_set.classifier, training_set.standardize_scalar
 
-def use_classifier(beacon_id, start_date, end_date, filter_window, classifier_name, label):
+def use_classifier(beacon_id, start_date, end_date, classifier_name):
 
 	classifier, gateway_list, standardize_scalar  = db.load_classifier(classifier_name)
 	
 	predicting_set = records.MatchedTimestamps()
-	predicting_set.init_from_database(beacon_id, gateway_list, start_date, end_date, 
-		filter_length=filter_window, label=label)
+	predicting_set.init_from_database(beacon_id, gateway_list, start_date, end_date)
 	predicting_set.classifier = classifier
 	predicting_set.replace_nan()
 	predicting_set.remove_nan()
