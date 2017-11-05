9e5465fc1c2a6fcbd571e818167491b576f8c9db
Alaa Shaalan <ashaalan@sfu.ca>
2017-11-05 15:54:56 -0800

    Add Logging

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,9 @@
 from init import app
+import logging
+
+handler = logging.Filehandler('/var/log/flask_app.log')
+handler.setLevel(logging.ERROR)
+app.logger.addHandler(handler)
 
 if __name__ == "__main__":
     app.run()
